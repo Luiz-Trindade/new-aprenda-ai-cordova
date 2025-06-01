e59ec7b93e43176b62ac93b65976959032483d46
Luiz-Trindade <luiz.gabriel.m.trindade@gmail.com>
2025-06-01 17:40:36 -0300

    feat(all): the app is ready i think

---
+++ www/assets/VSpacer-CWnj5cPZ.js
@@ -0,0 +1,1 @@
+import"./VContainer-BoOdDGmZ.js";import{d as r}from"./VCard-Bqd4Y468.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ www/assets/ssrBoot-DDsasX1I.js
@@ -0,0 +1,1 @@
+import{Q as t,D as e,t as s,aq as n}from"./index-BqceoyPB.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
